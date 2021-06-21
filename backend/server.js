07777f140b3893a1aebe0df82e335b150fa0675d
Potatomochi <noahrsam@gmail.com>
2021-06-21 17:44:13 +0800

    Initial

---
+++ backend/server.js
@@ -0,0 +1,70 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import userRouter from './routes/users.js';
+import authenticateRouter from './routes/auth.js';
+import postRouter from './routes/post.js';
+import path from 'path';
+import multer from 'multer';
+import data from './data.js';
+import User from './models/Users.js';
+import Post from './models/Post.js';
+
+
+dotenv.config();
+const app = express();
+
+const port  = 5000;
+
+mongoose.connect(
+    process.env.MONGO_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+      console.log("Connected to MongoDB");
+    }
+);
+const __dirname = path.resolve();
+console.log(__dirname)
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"))
+
+const storage = multer.diskStorage({
+    destination: (req,file,cb) => {
+        cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name);
+    },
+})
+
+const upload= multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file") , (req,res) => {
+    try{
+        return res.status(200).json("File submitted successfully.")
+    }catch(err){
+        res.status(400).send({error:"error, file could not be sent"})
+    }
+})
+
+
+app.use("/api/auth", authenticateRouter);
+app.use("/api/users", userRouter);
+app.use("/api/posts", postRouter);
+
+app.get('/', async (req,res) => {
+    const createdUsers = await User.insertMany(data.users);
+    const createdPosts = await Post.insertMany(data.posts);
+    res.send({createdUsers,createdPosts})
+} )
+
+
+
+app.listen(port , () =>{
+    console.log("Server is up")
+})
